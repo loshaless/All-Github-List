c7491608a308c4d1cc19b31cd992d2ee026a8c8d
loshaless <loshaless@gmail.com>
2021-06-16 01:17:50 +0700

    buat halaman Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Home() {
+  const [inputData, setInputData] = useState('')
+
+  function handleInputChange(event) {
+    setInputData(event.target.value)
+    console.log(inputData);
+  }
+
+  function submit() {
+
+  }
+
   return (
-    <div classNameName="container">
-      <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" class="img-thumbnail" alt="..."></img>
-      <div className="mb-3">
-        <label for="exampleFormControlInput1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-      </div>
-      <div className="mb-3">
-        <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <div className="container">
+      <div className="row justify-content-center align-content-center">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" style={{ maxWidth: "10vw" }} className="img-thumbnail mt-4" alt="..."></img>
+        <span className="row justify-content-center" style={{ fontSize: "3em" }}>Enter Your Username</span>
+        <div className="row justify-content-center mb-3">
+          <input type="username" className="form-control w-25" placeholder="loshaless" onChange={handleInputChange} />
+        </div>
+        <button type="button" class="btn btn-primary w-25" onChange={submit}>Submit</button>
       </div>
     </div>
   )
